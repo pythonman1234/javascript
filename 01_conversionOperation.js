fb87c958469664f1a1c37b29e0a81b6cc36bb47d
Mann Shah <107050272+pythonman1234@users.noreply.github.com>
2024-01-18 17:27:19 +0530

    Add files via upload

---
+++ 01_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "hitesh";
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+let isloggedIn = "Hitesh";
+
+let booleanIsLoggedIn = Boolean(isloggedIn);
+
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+let str1 = "hello";
+let str2 = "king";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
