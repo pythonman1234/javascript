001e745fbbd004d8e007d1deaff70b99e59b5a98
Mann Shah <107050272+pythonman1234@users.noreply.github.com>
2024-01-19 17:15:27 +0530

    Add files via upload

---
+++ date_type_summary.js
@@ -0,0 +1,24 @@
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id === anotherId);
+
+const heros = ["srk", "salman", "ranbir"];
+
+let myObj = {
+  name: "king",
+  age: 22,
+};
+
+const myFunction = function () {
+  console.log("hello world");
+};
+
+console.log(typeof anotherId);
