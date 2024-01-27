0c5a5338197d662c68bc92161a6d7d844c6c6e8b
Mann Shah <107050272+pythonman1234@users.noreply.github.com>
2024-01-27 07:39:32 +0530

    Add files via upload

---
+++ 05_iterations/one.js
@@ -0,0 +1,34 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+  const element = i;
+  if (element == 5) {
+    console.log("5 is the best number");
+  }
+  console.log(element);
+}
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`Outer loop value: ${i}`);
+  for (let j = 1; j <= 10; j++) {
+    console.log(`Inner loop values ${j} and inner loop is ${i}`);
+    console.log(i + "*" + j + "=" + i * j);
+  }
+}
+
+let myArray = ["flash", "batman", "superman"];
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
+}
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+  if (index == 5) {
+    console.log(`Detected 5`);
+    continue;
+  }
+  console.log(`Value of i is ${index}`);
+}
